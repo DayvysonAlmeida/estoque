0ad4de6132fff33bcb167151d7b693a07ba1d18d
Dayvyson Almneida <dayvysonalmeida@gmail.com>
2025-04-01 02:50:43 -0300

    alteração de segurança

--- backend/setup/settings.py
+++ backend/setup/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from datetime import timedelta
 from dotenv import load_dotenv
 
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hm66j=*8h5)s^yymq(5embo9ea9znxh6na#6f5(0@cj(3q3pp0'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
